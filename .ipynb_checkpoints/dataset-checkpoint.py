cf74e5492eb86b5160cc653e2610236df254f0d1
Rahul Avasarala <rahulavasarala@github.com>
2024-04-10 08:53:21 -0700

    ./serialize: Added Random Serialization
    added random serialization, so model
    can understand the meaning of signs better
    in the  training data
    
    Also the dataset files are now correctly generated

--- .ipynb_checkpoints/dataset-checkpoint.py
+++ .ipynb_checkpoints/dataset-checkpoint.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 import math
 import torch.nn.functional as F
 from torch.nn.init import xavier_uniform_
+import random as rand
+from serialization import random_serialize
 
 #Input: is the paths to the recursion sentences and solution sentences
 #Assume that the padding, start and end tokens are all the same ''
@@ -88,3 +90,72 @@ class RecursionDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.recursion_data[idx], self.solution_data[idx]
+
+def init_recursion_solution_files():
+
+    #pick a combination of a, b, and c from 0-9
+    # a will have to be non 0 for the solution to be generated
+
+    #generate the solution by writing it in an acceptable form
+    # for each pair a - b, randomly serialize the data each time,
+    #and create copies of the  input data
+    #repeat this until the dataset is 100 in size
+    #export the data and put it in the training cycle.
+
+    recursion_data = []
+    solution_data = []
+    
+    combo_map = {}
+
+    while len(recursion_data) < 100: 
+
+        a = rand.randint(1,9)
+        if a == 1:
+            a = 0
+        b = rand.randint(0,9)
+
+        c = rand.randint(0,9)
+    
+        combo = 100*a + 10 * b + c
+    
+        if combo in combo_map:
+            continue
+        else:
+            combo_map[combo] = True
+
+        recursion = "T_(n+1) = {} * T_n + {} * n + {}".format(a,b,c)
+
+        solution = "T_n = ({b}/(1-{a})) * n + ({c}/(1-{a})) - {b}/(1-{a})^2 + {d} * a ^ n".format(a = a, b = b, c = c, d = "d")
+
+        recursion_map = {}
+
+        batch_specific_recursions = []
+        batch_specific_solutions = []
+
+        while len(batch_specific_recursions) < 3:
+
+            rec_ser = random_serialize(recursion, random = True)
+            sol_ser = random_serialize(solution, random = True)
+            if rec_ser in recursion_map:
+                continue
+            else:
+                recursion_map[rec_ser] = True
+                batch_specific_recursions.append(rec_ser)
+                batch_specific_solutions.append(sol_ser)
+
+        recursion_data = recursion_data + batch_specific_recursions
+        solution_data = solution_data + batch_specific_solutions
+
+                
+
+    with open('recursions.txt', 'w') as f:
+        for line in recursion_data:
+            f.write("{}\n".format(line))
+
+    with open('solutions.txt', 'w') as f:
+        for line in solution_data:
+            f.write("{}\n".format(line))
+
+    return recursion_data, solution
+
+        
--- .ipynb_checkpoints/serialization-checkpoint.py
+++ .ipynb_checkpoints/serialization-checkpoint.py
@@ -1,43 +1,60 @@
-def serialize(expression):
+import random as rand
+
+def random_serialize(expression, random = True):
 
     expression = expression.replace(" ", "")
-
-    serialized_exp = serialize_helper(expression, 0, len(expression) -1 )
+    serialized_exp = random_serialize_helper(expression, 0, len(expression) -1, random)
 
     spaced_exp = " ".join(serialized_exp)
 
     return spaced_exp
+
+def random_serialize_helper(expression, start, end, random):
+
+    closed_index = find_closed_parenthesis(expression, start)
+    # print("closed index = {}".format(closed_index))
+
+    if expression[start] == '(' and closed_index == end:
+        return random_serialize_helper(expression, start+ 1, end -1, random)
+
+    sign_positions = find_sign_positions(expression, start, end)
+    # print("sign positions = {}".format(sign_positions))
+
+    if len(sign_positions) == 0:
+        sub = expression[start:end +1]
+        return [sub]
+    else:
+        #randomly pick one sign and do the partition along there
+
+        sign_pos = sign_positions[0]
+
+        if random:
+            num_positions = len(sign_positions)
+            idx = rand.randint(0, num_positions - 1)
+            sign_pos = sign_positions[idx]
+
+        # print(num_positions, idx, sign_pos)
     
-def serialize_helper(expression, start, end):
+        sign = expression[sign_pos]
+        sub1 = random_serialize_helper(expression, start, sign_pos -1, random)
+        sub2 = random_serialize_helper(expression, sign_pos + 1, end, random)
 
-    if expression[start] == '(':
-
-        closed_index = find_closed_parenthesis(expression, start)
-        if closed_index == end: #This means that there are no signs
-            return serialize_helper(expression, start+ 1, end -1)
-        elif closed_index == -1:
-            raise Exception("No matching parenthesis found!")
-        else:
-            sign_index = find_sign(expression, closed_index + 1, end)
-            sub1 = serialize_helper(expression, start, sign_index -1)
-            sign = "" + expression[sign_index]
-            sub2 = serialize_helper(expression, sign_index + 1, end)
-            return [sign] + sub1 + sub2
-    else:#If the expression starts with a number
-        sign_index = find_sign(expression, start + 1, end)
-        if sign_index == -1:
-            sub = expression[start:end + 1]
-            return [sub]
-        else:
-            sub1 = serialize_helper(expression, start, sign_index -1)
-            sign = "" + expression[sign_index]
-            sub2 = serialize_helper(expression, sign_index + 1, end)
-            return [sign] + sub1 + sub2 
+        if random:
+            coin_flip = rand.randint(0,1)
             
+            if coin_flip == 1 and sign in "+*": 
+                return [sign] + sub2 + sub1
+            else:
+                return [sign] + sub1 + sub2
 
+        return [sign] + sub1 + sub2
+    
 def find_closed_parenthesis(expression, start):
 
     open_count = 0
+
+    if expression[start] != '(':
+        return -1
 
     for i in range(start + 1, len(expression)):
 
@@ -53,29 +70,6 @@ def find_closed_parenthesis(expression, start):
 #This find sign will need to turn into find viable signs
 #need to update this method to work on expressions with parenthesis
 
-def find_sign(expression, start, end):
-
-    for i in range(start, end + 1):
-
-        if expression[i] == '(':
-            break
-        elif expression[i] in "+-":
-            return i
-
-    for i in range(start, end + 1):
-        if expression[i] == '(':
-            break
-        elif expression[i] in "*/":
-            return i
-
-    for i in range(start, end + 1):
-        if expression[i] == '(':
-            break
-        elif expression[i] in "^":
-            return i
-
-    return -1 #-1 will never be returned if closed index != end
-
 def find_sign_positions(expression, start, end):
 
     sign_priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '_': 4, '=': 0}
@@ -85,8 +79,10 @@ def find_sign_positions(expression, start, end):
 
     i = start
 
+    if expression[i] == '-':
+        i += 1
+
     while i < end + 1:
-        print(i)
         if expression[i] == '(':
             closed = find_closed_parenthesis(expression, i)
             i = closed
@@ -105,21 +101,22 @@ def unserialize(expression):
     expression_list = expression.split()
 
     big_tree_end = find_tree_end(expression_list, 0)
+    print(big_tree_end)
 
     if big_tree_end != len(expression_list) - 1:
         print("Expression is invalid!")
 
-    unserialized_expression = unserialize_helper(expression_list, 0, len(expression_list) -1)\
+    unserialized_expression = unserialize_helper(expression_list, 0, len(expression_list) -1)
 
     return unserialized_expression
 
 
 def unserialize_helper(expression_list, start, end):
 
-    if start == end and expression_list[start] not in "*/+-^":
+    if start == end and expression_list[start] not in "*/+-^_=":
         return expression_list[start]
 
-    if expression_list[start] in "*/+-^":
+    if expression_list[start] in "*/+-^_=":
         tree_end = find_tree_end(expression_list, start + 1)
 
         left_expression = unserialize_helper(expression_list, start + 1, tree_end)
@@ -131,14 +128,14 @@ def unserialize_helper(expression_list, start, end):
     
 def find_tree_end(expression_list, start):
 
-    if expression_list[start] not in "*/+-^":#if the start is just a number, the tree end is the index itself
+    if expression_list[start] not in "*/+-^_=":#if the start is just a number, the tree end is the index itself
         return start
 
     open_count = 2
 
     for i in range(start + 1, len(expression_list)):
 
-        if expression_list[i] not in "*/+-^":
+        if expression_list[i] not in "*/+-^_=":
             open_count -= 1
         else:
             open_count += 1
